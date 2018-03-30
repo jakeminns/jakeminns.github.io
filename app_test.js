8ba94e376764a7aaf34394deeb8a5b4095d1a7cc
jakeLminns <jm648@kent.ac.uk>
2018-03-30 21:46:26 +0100

    dd

--- app_test.js
+++ app_test.js
@@ -6,21 +6,23 @@ var x = 0;
 var y = 0;
 
 var mainDiv= document.getElementById("thingid");
-var widthHalf = 1000;
 
-    $.getJSON("https://api.thingiverse.com/users/jkminns/things?access_token=46d84caf6cbf8d4afc2215b32d758854",function(result){
-        
-        $.each(result, function(i, item){
-        
-        console.log(result[i]);
+var widthHalf = mainDiv.offsetWidth/2;
 
-		div.style.background =  "url('" +result[i].thumbnail+ "')";
+var numCols = widthHalf/220
+console.log(numCols)
+
+for (i = 0; i < 20; i++) { 
+
+		var div = document.createElement("div");
+
+		
+		div.style.background =  "url('https://cdn.thingiverse.com/renders/19/a8/3a/e3/53/b2d47907ec54d4b0dff5b9f8032a00e5_thumb_large.jpg')";
 		div.style.backgroundRepeat = "no-repeat";
 		div.style.backgroundSize="cover"
 		div.style.color = "white";
 		div.setAttribute("id","3dObj_"+i)
 		div.setAttribute("class","col-md-2 col-s-2 col-sm-2")
-		div.setAttribute("href",result[i].public_url)
 
 		div.style.height = "16.66666vw";
 
@@ -28,9 +30,5 @@ var widthHalf = 1000;
 
 
 
-
-      });
-
-    });
-
+}
 
