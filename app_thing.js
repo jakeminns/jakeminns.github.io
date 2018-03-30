7e8b235fc41b3bd904282fd503c8e4afe8a2075b
jakeLminns <jm648@kent.ac.uk>
2018-03-30 19:38:59 +0100

    obj

--- app_thing.js
+++ app_thing.js
@@ -5,21 +5,25 @@ var xhr = new XMLHttpRequest();
 var x = 0;
 var y = 0;
 
-var mydiv = document.getElementById("thingid");
+var mainDiv= document.getElementById("thingid");
 var widthHalf = 1000;
 
     $.getJSON("https://api.thingiverse.com/users/jkminns/things?access_token=46d84caf6cbf8d4afc2215b32d758854",function(result){
-
-
-        console.log(result[1].thumbnail);
+        
         $.each(result, function(i, item){
+        
         console.log(result[i]);
- 		document.getElementById(thingid).style.background = "url('" +result[i].thumbnail+ "')";
- 		                 x=x+1;
 
+		var div = document.createElement("div");
+		div.style.width = "100px";
+		div.style.height = "100px";
+		div.style.background =  "url('" +result[i].thumbnail+ "')";
+		div.style.color = "white";
+		div.setAttribute("id","3dObj_"+i)
+		mainDiv.appendChild(div)
 
-                 return false;
-                 return false;
+        return false;
+        return false;
 
       });
 
